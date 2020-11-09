900c0458d6c77429c4043027c24f720ff19b0a3b
Ztowne13 <ztowne13@gmail.com>
2020-11-09 14:19:51 -0800

    Updates

--- main.cpp
+++ main.cpp
@@ -1,5 +1,21 @@
 #include "main.h"
 
 int main(int argc, char** argv) {
+	if(argc == 1) {
+		std::cout << "usage:" << std::endl;
+		std::cout << "  -g | --generate: Generate a random public/private key" << std::endl;
+	} else {
+		for(int i = 1; i < argc; i++) {
+			if(std::string(argv[i]) == "-g" || std::string(argv[i]) == "--generate") {
+				generatePrivateKey();
+			}
+		}
+	}
 	return 1;
+}
+
+std::string generatePrivateKey() {
+	std::cout << rand(123456789) << std::endl;
+
+	return "1";
 }
--- main.h
+++ main.h
@@ -1,6 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+#include <iostream>
+
 int main(int argc, char** argv);
+std::string generatePrivateKey();
 
 #endif //MAIN_H
